6d57ffac54f65a3d6c2d41bdf469c65b5b61c63f
Stella-Remember <139786218+Stella-Remember@users.noreply.github.com>
2024-06-15 17:22:56 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""Start a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    - /: Displays 'Hello HBNB!'
-    - /hbnb: Displays 'HBNB'
-    - /c/<text>: Displays 'C' followed by the value of the text variable
-"""
 
+"""Script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -14,21 +8,27 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display a message"""
-    return 'Hello HBNB!'
+    """Comment"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Display a HBNB"""
-    return 'HBNB'
+    """Comment"""
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Display C followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+def text_route(text):
+    """Comment"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def text_route_python(text="is cool"):
+    """Comment"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
