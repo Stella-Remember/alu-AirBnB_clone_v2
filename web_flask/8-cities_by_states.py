363a33f06ad865f6221a691173068f1fbce9e7eb
Stella-Remember <139786218+Stella-Remember@users.noreply.github.com>
2024-06-16 21:21:38 +0200

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,7 @@
 #!/usr/bin/python3
-"""
-Your web application must be listening on 0.0.0.0, port 5000
-You must use storage for fetching data from the storage engine
-(FileStorage or DBStorage) => from models import storage and storage.all(...)
-To load all cities of a State:
-If your storage engine is DBStorage, you must use cities relationship
-Otherwise, use the public getter method cities
-After each request you must remove the current SQLAlchemy Session:
-Declare a method to handle @app.teardown_appcontext
-Call in this method storage.close()
-Routes:
-/cities_by_states: display a HTML page: (inside the tag BODY)
-H1 tag: “States”
-UL tag: with the list of all State objects present in DBStorage sorted by name
-(A->Z) tip
-LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag:
-with the list of City objects linked to the State sorted by name (A->Z)
-LI tag: description of one City: <city.id>: <B><city.name></B>
-Import this 7-dump to have some data
-You must use the option strict_slashes=False in your route definition
-"""
+
+"""Starts a Flask web application"""
+
 from models import storage
 from models.state import State
 from flask import Flask
@@ -43,4 +25,3 @@ def teardown(self):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
